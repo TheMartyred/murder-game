7efb864341ed94d30e8ec8cee78e538aa1f8b8e0
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-10-20 04:41:21 -0400

    added images

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ var fs = require('fs');
 var known = ["yoshi", "fox"]
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, 
-{
+logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
@@ -15,14 +14,12 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) 
-{
+bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) 
-{
+bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 8) == "I'd tap ") 
